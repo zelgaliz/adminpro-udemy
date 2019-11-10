b07c8e8f81b6c818698ff96c7626c80b08286d22
unknown <daniel.trejos@cecar.edu.co>
2019-11-10 10:58:45 -0500

    modificacion en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,32 +2,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PagesComponent } from './pages/pages.component';
 
-import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-
+import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent ,
-        children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progess', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-        ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-       // { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
     { path: '**', component: NopagefoundComponent }
+    // {
+    //     path: '',
+    //     component: PagesComponent ,
+    //     children: [
+    //         { path: 'dashboard', component: DashboardComponent},
+    //         { path: 'progess', component: ProgressComponent },
+    //         { path: 'graficas1', component: Graficas1Component },
+    //         { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+    //     ]
+    // },
+       // { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
 ];
-
-// tslint:disable-next-line: eofline
 export const APP_ROUTES = RouterModule.forRoot( appRoutes, {useHash : true} );
