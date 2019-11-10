f70f5e53453ebb9ae040a7e751a58406c820b611
unknown <daniel.trejos@cecar.edu.co>
2019-11-10 10:59:41 -0500

    agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import {  NgModule} from '@angular/core';
+
+import { SharedModule } from './../shared/shared.module';
+
+import { PagesComponent } from './pages.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent,
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent,
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+
+    ]
+})
+
+export class PagesModule {}
+
